9522f80131c8f75fc78bf6f61768fd8eb3affce6
yu.tang <tangyu.dch@gmail.com>
2026-06-03 16:12:10 +0800

    fix: dynamically translate Unix paths in pkg-config bat wrapper to windows native format

--- build/pkg-config-static.bat
+++ build/pkg-config-static.bat
@@ -1,2 +1,9 @@
 @echo off
-pkg-config --static %*
+setlocal enabledelayedexpansion
+for /f "delims=" %%i in ('pkg-config --static %*') do (
+    set "line=%%i"
+    set "line=!line:/C/pjsip-install=C:/pjsip-install!"
+    set "line=!line:/c/pjsip-install=C:/pjsip-install!"
+    echo !line!
+)
+endlocal
